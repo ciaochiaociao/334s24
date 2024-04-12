05214db79c90dedcd66deb6b61c6196ca532ffb3
Chiao-Wei Hsu <cwhsu@OMEN>
2024-04-11 21:03:28 -0500

    remove tests

--- src/transaction.rs
+++ src/transaction.rs
@@ -29,18 +29,8 @@ pub fn verify(t: &RawTransaction, public_key: &<Ed25519KeyPair as KeyPair>::Publ
 mod tests {
     use super::*;
     use crate::crypto::key_pair;
-    use log::info;
-    use std::sync::Once;
 
-    static INIT: Once = Once::new();
-
-    fn setup() {
-        INIT.call_once(|| {
-            env_logger::builder().is_test(true).try_init().unwrap();
-        });
-    }
     pub fn generate_random_transaction() -> RawTransaction {
-        info!("Generating random transaction!!!!!!!!!!!\n");
         RawTransaction {
             input: rand::random::<u32>(),
             output: rand::random::<u32>(),
@@ -49,8 +39,6 @@ mod tests {
 
     #[test]
     fn sign_verify() {
-        setup();
-        info!("sign_verify test");
         let t = generate_random_transaction();
         let key = key_pair::random();
         let signature = sign(&t, &key);
@@ -61,25 +49,4 @@ mod tests {
         assert!(verify(&t, &(key.public_key()), &signature));
     }
 
-    #[test]
-    fn another_sign_verify() {
-        setup();
-        info!("another_sign_verify test");
-        let t = generate_random_transaction();
-        let key = key_pair::random();
-        let signature = sign(&t, &key);
-        assert!(verify(&t, &(key.public_key()), &signature));
-    }
-
-    #[test]
-    fn sign_verify_fail() {
-        setup();
-        info!("sign_verify_fail test");
-        let t = generate_random_transaction();
-        let key = key_pair::random();
-        let signature = sign(&t, &key);
-        let mut t2 = t.clone();
-        t2.output += 1;
-        assert!(!verify(&t2, &(key.public_key()), &signature));
-    }
 }
