9b1ce8cab2b9b5f5851b6d3cb973804321747afb
Chiao-Wei Hsu <cwhsu@OMEN>
2024-04-27 05:00:17 -0500

    add missing tests

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -78,4 +78,47 @@ mod tests {
         assert_eq!(blockchain.tip(), block.hash());
 
     }
+
+    #[test]
+    fn mp1_insert_chain() {
+        let mut blockchain = Blockchain::new();
+        let genesis_hash = blockchain.tip();
+        let mut block = generate_random_block(&genesis_hash);
+        blockchain.insert(&block);
+        assert_eq!(blockchain.tip(), block.hash());
+        for _ in 0..50 {
+            let h = block.hash();
+            block = generate_random_block(&h);
+            blockchain.insert(&block);
+            assert_eq!(blockchain.tip(), block.hash());
+        }
+    }
+
+    #[test]
+    fn mp1_insert_3_fork_and_back() {
+        let mut blockchain = Blockchain::new();
+        let genesis_hash = blockchain.tip();
+        let block_1 = generate_random_block(&genesis_hash);
+        blockchain.insert(&block_1);
+        assert_eq!(blockchain.tip(), block_1.hash());
+        let block_2 = generate_random_block(&block_1.hash());
+        blockchain.insert(&block_2);
+        assert_eq!(blockchain.tip(), block_2.hash());
+        let block_3 = generate_random_block(&block_2.hash());
+        blockchain.insert(&block_3);
+        assert_eq!(blockchain.tip(), block_3.hash());
+        let fork_block_1 = generate_random_block(&block_2.hash());
+        blockchain.insert(&fork_block_1);
+        assert_eq!(blockchain.tip(), block_3.hash());
+        let fork_block_2 = generate_random_block(&fork_block_1.hash());
+        blockchain.insert(&fork_block_2);
+        assert_eq!(blockchain.tip(), fork_block_2.hash());
+        let block_4 = generate_random_block(&block_3.hash());
+        blockchain.insert(&block_4);
+        assert_eq!(blockchain.tip(), fork_block_2.hash());
+        let block_5 = generate_random_block(&block_4.hash());
+        blockchain.insert(&block_5);
+        assert_eq!(blockchain.tip(), block_5.hash());
+    }
+
 }
